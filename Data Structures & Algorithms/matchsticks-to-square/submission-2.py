fc1e8c7feb105f97545b2771f522d4e44a185bab
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-05-07 00:33:55 -0700

    Add: matchsticks-to-square - submission-2

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        sides = [0, 0, 0, 0]
+        target = sum(matchsticks) // 4
+        cur = 0
+        if sum(matchsticks) % 4 != 0:
+            return False
+
+        # sorting them makes it explore far fewer combos
+        matchsticks.sort(reverse=True)
+        return self.recurse(matchsticks, sides, cur, target)
+
+
+    def recurse(self, matchsticks, sides, cur, target): 
+        if cur == len(matchsticks):
+            for side in sides:
+                if side != target:
+                    return False
+            return True
+        
+
+        matchstick = matchsticks[cur]
+
+        # try placing in the 4 sides
+        for j in range(4):
+            # choose
+            sides[j] += matchstick
+            
+            # explore
+            if sides[j] <= target:
+                if self.recurse(matchsticks, sides, cur + 1, target):
+                    return True
+
+            # un choose
+            sides[j] -= matchstick
+         
+        return False
+        
