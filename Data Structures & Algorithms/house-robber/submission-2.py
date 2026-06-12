c3035c4adf558b1cad4fefb3374f338129370648
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-06-12 08:39:15 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        # memo represents the max amount of money we can have at
+        # each hosue
+
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+
+        # only need to track two values so 
+        # this is a space optmized version
+        # time is O(n), space is O(1)
+        memo = [0] * 2
+
+        memo[0] = nums[0]
+        memo[1] = max(nums[0], nums[1])
+
+        
+        for i in range(2, len(nums)):
+            val = max(memo[1], memo[0] + nums[i])
+            memo[0] = memo[1]
+            memo[1] = val
+
+
+        return memo[1]
+
+            
+
+        
