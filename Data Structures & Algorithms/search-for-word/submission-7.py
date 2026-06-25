f071e6508b6883f8ea8cce21afb7795959727646
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-06-24 19:32:21 -0700

    Add: search-for-word - submission-7

---
+++ Data Structures & Algorithms/search-for-word/submission-7.py
@@ -0,0 +1,36 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        for y in range(len(board)):
+            for x in range(len(board[0])):
+                if self.recurse(board, word, x, y, [], set()):
+                    return True
+
+        return False    
+
+    
+    def recurse(self, board, word, x, y, cur, visited):
+
+        if x >= len(board[0]) or y >= len(board) or x < 0 or y < 0:
+            return False
+
+        if (x, y) in visited:
+            return False
+
+        cur.append(board[y][x])
+
+        visited.add((x, y))
+
+        if "".join(cur) == word:
+            return True
+        
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        for dx, dy in directions:
+            if self.recurse(board, word, x + dx, y + dy, cur, visited):
+                return True
+
+        visited.remove((x, y))
+        cur.pop()
+        return False
+        
+        
