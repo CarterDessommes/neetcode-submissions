908ae299082fa453fd23dadef9094ead05179ddb
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-07-30 20:50:09 -0700

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # reverse + rotation = 90 degree rotations
+        # good trick to remember!
+
+        # reverse order of rows
+        matrix.reverse()
+
+        # loop over everything above the diagonal
+        for r in range(len(matrix)):
+            for c in range(r + 1, len(matrix[0])):
+                # switch everything across the diagnonal, performing transpose
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        
+
+
