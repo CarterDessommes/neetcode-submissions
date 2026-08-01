6c01aaef4eddc2ce067ebd3fdb46d93de93096da
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-08-01 13:25:24 -0700

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        res = []
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        #steps[0] is how many moves left in the horizontal direction
+        #steps[1] is how many moves left in the vertical direction
+        steps = [len(matrix[0]), len(matrix) - 1]
+        
+
+        # d is direction, 0, 1, 2, or 3
+        # 0 is right, 1 is down, 2 is left, 3 is up
+        r, c, d = 0, -1, 0
+
+        # this is a cool trick to 
+        # alter which index of steps we are looking at
+        # so if the direction is right, or 0, we look at 0th index.
+        # and so on
+        while steps[d & 1]:
+            # move forward as far as we can
+            for i in range(steps[d & 1]):
+                # moving in our current direction each time
+                r += directions[d][0]
+                c += directions[d][1]
+                res.append(matrix[r][c])
+            # effectviely peel off one layer
+            steps[d & 1] -= 1
+            # change direction
+            d += 1 
+            d %= 4
+        return res
+
+
+        
+
+        
