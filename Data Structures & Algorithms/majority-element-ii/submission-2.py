0d578477f5dced834f9e804b3eda3c2f9104fec3
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-08-16 22:52:16 -0700

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,50 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        cnt1 = cnt2 = 0
+        num1 = num2 = -1
+
+        for num in nums:
+            if num == num1:
+                cnt1 += 1
+            elif num == num2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                num1 = num
+                cnt1 = 1
+            elif cnt2 == 0:
+                num2 = num
+                cnt2 = 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        
+        cnt1 = cnt2 = 0
+        for num in nums:
+            if num == num1:
+                cnt1 += 1
+            elif num == num2:
+                cnt2 += 1
+            
+        res = []
+        if cnt1 > n // 3:
+            res.append(num1)
+        if cnt2 > n // 3:
+            res.append(num2)
+        
+        return res
+
+        # target = len(nums) // 3
+
+        # counts = Counter(nums)
+
+        # sol = []
+        # for val in counts:
+        #     if counts[val] > target:
+        #         sol.append(val)
+        
+        # return sol
+        
+        # o(1) space uses boyer moore voting just tracking two elements
+        #
