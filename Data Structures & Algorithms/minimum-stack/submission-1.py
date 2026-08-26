c3cd1974d22273fd0ae88d708f1d7e4efe25e5fd
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-08-25 21:43:14 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minVal = 0
+
+        
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append(0)
+            self.minVal = val
+        else:
+            self.stack.append(val - self.minVal)
+            self.minVal = min(self.minVal, val)
+
+    def pop(self) -> None:
+        encoded = self.stack.pop()
+        if encoded < 0:
+            self.minVal = self.minVal - encoded
+        
+        
+    def top(self) -> int:
+        encoded = self.stack[-1]
+        if encoded > 0:
+            return encoded + self.minVal
+        else:
+            return self.minVal
+
+    def getMin(self) -> int:
+        return self.minVal
+        
+        
