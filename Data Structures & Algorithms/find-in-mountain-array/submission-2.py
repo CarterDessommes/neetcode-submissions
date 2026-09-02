3e440ba7db5e791b683ac0f3be24770b47b10bc7
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-09-02 11:22:29 -0700

    Add: find-in-mountain-array - submission-2

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-2.py
@@ -0,0 +1,80 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 
+        'MountainArray') -> int:
+
+
+        # run binary serach three times, once to find peak
+        # then two more times to look on the left and right side
+        # of the peak, noting that the righty side is 
+        # sorteed descending so the logix is flippped.
+
+
+        l = 1
+        r = mountainArr.length() - 2
+
+        while l <= r:
+            m = (l + r) // 2
+
+            valL = mountainArr.get(m-1)
+            valR = mountainArr.get(m+1)
+            valM = mountainArr.get(m)
+
+            # in left side
+            if valL < valM < valR:
+                l = m + 1
+            # in right side    
+            elif valL > valM > valR:
+                r = m - 1
+            else:
+                break
+        
+        peak = m
+
+        l = 0
+        r = peak - 1
+
+        # serach left part of array 
+        # ascending
+        while l <= r:
+            m = (l + r) // 2
+
+            val = mountainArr.get(m)
+
+            # in left side
+            if val < target:
+                l = m + 1
+            # in right side    
+            elif val > target:
+                r = m - 1
+            else:
+                return m
+        
+        l = peak
+        r = mountainArr.length() - 1
+
+        # right part of the array is ascending
+        while l <= r:
+            m = (l + r) // 2
+
+            val = mountainArr.get(m)
+
+            # in left side
+            if val < target:
+                r = m - 1
+            # in right side    
+            elif val > target:
+                l = m + 1
+            else:
+                return m
+        
+        return -1
+
+
+
+            
+
+
+
+
+
+        
