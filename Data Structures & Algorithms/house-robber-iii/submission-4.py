afaaa602904ebe2f711305b79575e64e29fd4a6d
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-09-07 19:41:39 -0700

    Add: house-robber-iii - submission-4

---
+++ Data Structures & Algorithms/house-robber-iii/submission-4.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+
+        cache = {}
+
+        def dfs(root):
+            if root in cache:
+                return cache[root]
+        
+            if not root:
+                return 0
+            
+            cache[root] = root.val
+            if root.left:
+                cache[root] += dfs(root.left.left)
+                cache[root] += dfs(root.left.right)
+            
+            if root.right:
+                cache[root] += dfs(root.right.right)
+                cache[root] += dfs(root.right.left)
+            
+            cache[root] = max(cache[root], dfs(root.right) + dfs(root.left))
+            return cache[root]
+        
+        return dfs(root)
