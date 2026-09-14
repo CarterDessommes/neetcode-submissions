d2fee6186ef62e2e150cc21c4e43845399692757
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-09-13 19:19:32 -0700

    Add: reorganize-string - submission-2

---
+++ Data Structures & Algorithms/reorganize-string/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = [0] * 26
+        prev = -1
+        res = []
+
+        for char in s:
+            count[ord(char) - ord("a")] += 1
+        
+        def getMax(prev):
+            maxCnt = -1
+            idx = -1
+            for i in range(26):
+                if i == prev or count[i] == 0:
+                    continue
+                
+                if count[i] > maxCnt:
+                    maxCnt = count[i]
+                    idx = i
+            
+            return idx
+        
+        while True:
+            idx = getMax(prev)
+
+            if idx == -1:
+                break
+            
+            res.append(chr(idx + ord("a")))
+            count[idx] -= 1
+
+            prev = idx
+        
+        return "".join(res) if len(res) == len(s) else ""
+            
+
+
+
+        
+
+
+
+        
