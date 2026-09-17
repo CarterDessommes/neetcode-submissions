d8d5e1e47b80d369cb46b3bbedd1ed6c104f9bcd
CarterDessommes <154269744+CarterDessommes@users.noreply.github.com>
2026-09-17 13:35:22 -0700

    Add: palindrome-partitioning - submission-4

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-4.py
@@ -0,0 +1,38 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+
+        # precompute palindromic substrings to save time.
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]        
+        for center in range(n):
+            # test even and odd centers
+            for left, right in [(center, center), (center, center+1)]:
+                while left >= 0 and right < n and s[left] == s[right]:
+                    dp[left][right] = True
+                    left -= 1
+                    right += 1
+        
+        
+        res = []
+        cur = []
+        
+        def dfs(i, j):
+            if j >= len(s):
+                if i == j:
+                    # n * 2^n due to this
+                    res.append(cur.copy())
+                return 
+
+            if dp[i][j]:
+                # choose explore unchoose
+                cur.append(s[i : j + 1])
+                dfs(j + 1, j + 1)
+                cur.pop()
+            
+            dfs(i, j + 1)
+        
+        dfs(0, 0)
+        return res
+        ###
+        
+        
